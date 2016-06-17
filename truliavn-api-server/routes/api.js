a97aa6cac98cf5dfb62813de1ca5820a197d6ee5
Ngoc Don <kaitoukid2701@gmail.com>
2016-06-17 12:57:39 +0700

    Add login API

--- truliavn-api-server/routes/api.js
+++ truliavn-api-server/routes/api.js
@@ -5,6 +5,9 @@ var uploadImages = multer({dest: 'public/uploads/images'});
 var fs = require('fs-extra');
 var async = require('async');
 var mysql = require('mysql');
+var bcrypt = require('bcrypt-nodejs');
+var CryptoJS = require('crypto-js');
+
 var connection = mysql.createConnection({
 	host     : 'localhost',
 	user     : 'root',
@@ -42,6 +45,14 @@ var HOUSE_STATUS = {
 router.get('/', function(req, res) {
 	res.json({'status': 'success'});
 });
+
+/**
+ * ======================
+ *
+ * API for House operation
+ *
+ * ======================
+ */
 
 /**
  * Get information of a house
@@ -424,6 +435,9 @@ router.post('/house/edit', uploadImages.array('images'), function (req, res) {
 	)
 })
 
+/**
+ * Get all saved features
+ */
 router.get('/getfeatures', function (req, res) {
 	connection.query(
 		'SELECT * FROM Features',
@@ -444,6 +458,136 @@ router.get('/getfeatures', function (req, res) {
 		}
 	)
 })
+
+/**
+ * ======================
+ *
+ * API for User operation
+ *
+ * ======================
+ */
+
+/**
+ * Register
+ */
+router.post('/register', uploadImages.single('photo'), function (req, res) {
+	console.log(req.body);
+	var password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(8), null);
+	connection.query(
+		'SELECT * FROM Users WHERE email = ?',
+		[req.body.email],
+		function (err, users, fields) {
+			if (err){
+				res.json({
+					status: 'error',
+					error: 'Error while reading database'
+				})
+			}
+			if (users.length > 0){
+				res.json({
+					status: "error",
+					error: 'User has already existed'
+				});
+				return;
+			}
+
+			var rb = req.body;
+			var token = makeToken(rb.email);
+			console.log(password);
+			console.log(token);
+
+			connection.query(
+				'INSERT INTO Users (email, password, fullname, phone, address, token) VALUES (?, ?, ?, ?, ?, ?)',
+				[rb.email, password, rb.name, rb.phone, rb.address, token],
+				function (error, result) {
+					if (error){
+						console.log(error);
+						res.json({
+							status: 'error',
+							error: 'Error while writing on database'
+						});
+						return
+					}
+					res.json({
+						status: 'success',
+						user: {
+							email: rb.email,
+							name: rb.name,
+							token: token
+						}
+					})
+
+				}
+			)
+
+		}
+	)
+})
+
+router.post('/login', uploadImages.single('photo'), function (req, res) {
+	connection.query(
+		'SELECT * FROM Users WHERE email = ?',
+		[req.body.email],
+		function (err, users, fields) {
+			if (err){
+				console.log(err);
+				res.json({
+					status: 'error',
+					error: 'Error while reading database'
+				});
+				return
+			}
+			if (users.length < 1){
+				res.json({
+					status: 'error',
+					error: 'Invalid email'
+				});
+				return;
+			}
+			var user = users[0];
+			if (!bcrypt.compareSync(req.body.password, user.password)){
+				res.json({
+					status: 'error',
+					error: 'Invalid password'
+				});
+
+			}
+			var token = makeToken(user.email);
+			// renew token
+			connection.query(
+				'UPDATE Users SET token = ? WHERE id = ?',
+				[token, user.id],
+				function (err, result) {
+
+					// if err, use old token
+					if (err){
+						console.log(err);
+						res.json({
+							status: 'success',
+							user: {
+								email: user.email,
+								fullname: user.fullname,
+								token: user.token
+							}
+						});
+					}
+					else{
+						res.json({
+							status: 'success',
+							user: {
+								email: user.email,
+								fullname: user.fullname,
+								token: token
+							}
+						})
+					}
+				}
+			)
+			
+		}
+	)
+})
+
 
 /* debugging API */
 
@@ -484,6 +628,10 @@ router.put('*', function (req, res) {
 router.delete('*', function (req, res) {
 	res.json({'status': 'error', 'error': 'Invalid API endpoint.'});
 })
+
+function makeToken (email) {
+	return CryptoJS.MD5(email + bcrypt.genSaltSync(100)).toString();
+}
 
 function deleteImagesOfHouse (houseId, fn) {
 	connection.query(
