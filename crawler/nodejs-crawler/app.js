5e8a420e6c3250421a1129e91334b2c1f490952a
Ngoc Don <kaitoukid2701@gmail.com>
2016-06-20 01:36:28 +0700

    update Crawler

--- crawler/nodejs-crawler/app.js
+++ crawler/nodejs-crawler/app.js
@@ -10,11 +10,30 @@ var length = urls.length;
 	// crawl(i);
 // }
 
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: '',
+	database: 'truliavn'
+});
+
+var results = [];
+
+String.prototype.myTrim = function() {
+	var s = this.trim();
+	s = s.replace(/\r+\n+/g, ' ');
+	s = s.replace(/ {2,}/g, ' ');
+	return s;
+}
+
 crawl(0);
 
 function crawl (index) {
 	if (index >= length){
 		console.log('done.');
+		fs.writeFileSync('data.json', JSON.stringify(results, null, 4));
+		saveToDB(0);
 		return;
 	}
 	console.log("Start crawling from:\n\n" + urls[index] + "\n");
@@ -38,22 +57,29 @@ function crawl (index) {
 				}
 
 				div = $(div);
-				// console.log(div.children('.left').text().trim());
-				switch (div.children('.left').text().trim()){
+				// console.log(div.children('.left').text().myTrim());
+				switch (div.children('.left').text().myTrim()){
 					case "Địa chỉ":
-						result.address = div.children('.right').text().trim();
+						result.address = div.children('.right').text().myTrim();
 						break;
 					case "Số phòng ngủ":
-						result.bedrooms = parseInt(div.children('.right').text().trim());
+						result.bedrooms = parseInt(div.children('.right').text().myTrim());
 						break;
 					case "Số toilet":
-						result.bathrooms = parseInt(div.children('.right').text().trim());
+						result.bathrooms = parseInt(div.children('.right').text().myTrim());
 						break;
 					case "Nội thất":
-						result.interior = div.children('.right').text().trim();
+						result.interior = div.children('.right').text().myTrim();
 						break;
 					case "Số tầng":
-						result.floors = parseInt(div.children('.right').text().trim());
+						result.floors = parseInt(div.children('.right').text().myTrim());
+						break;
+					case "Mặt tiền":
+						result.frontend = div.children('.right').text().myTrim();
+						break;
+					case "Đường vào":
+						result.entrance = div.children('.right').text().myTrim();
+						break;
 				}
 
 			}
@@ -62,24 +88,123 @@ function crawl (index) {
 			for (var i = 0; i < houseInfo.length; i++) {
 				var element = $(houseInfo[i]);
 				// console.log(element.children().eq(0).text());
-				if (element.children().eq(0).text().trim() == 'Giá:'){
-					result.price = element.children('strong').text().trim();
+				if (element.children().eq(0).text().myTrim() == 'Giá:'){
+					result.price = element.children('strong').text().myTrim();
 				}
-				else if (element.children('b').text().trim() == 'Diện tích:'){
-					result.area = element.children('strong').text().trim();
+				else if (element.children('b').text().myTrim() == 'Diện tích:'){
+					result.area = element.children('strong').text().myTrim();
 				}
 			}
-			// result.description = unescape($('.pm-content.stat').text().replace(/<div(.|\n|\r)*<\/div>/g, '').trim());
-			var des = $('.pm-content.stat').text().trim().replace(/< ?\/? ?br ?\/? ?>/g, "\n")
+			// result.description = unescape($('.pm-content.stat').text().replace(/<div(.|\n|\r)*<\/div>/g, '').myTrim());
+			var des = $('.pm-content.stat').html().myTrim();
+			des = des.replace(/< ?\/? ?br ?\/? ?>/gi, '\n')
+			des = des.replace(/<div(.|\n|\r)*<\/div>/g, '').myTrim();
 			// des = des.replace(/< ?\/? ?br ?\/? ?>/g, "\n");
 			// des = $(des).text();
 			// console.log(des);
-			result.description = des.replace(/Tìm kiếm theo từ khóa:(.|\n|\r)*$/g, '').trim();
-			console.log(result);
+			des = des.replace(/Tìm kiếm theo từ khóa:(.|\n|\r)*$/g, '').myTrim();
+
+			result.description = hex2str(des);
+			// result.description = des;
+
+			// crawl owner info
+			var owner = {};
+			var ownerInfo = $('#divCustomerInfo').children();
+			for (var i = 0; i < ownerInfo.length; i++) {
+				var div = ownerInfo[i];
+				if (div.attribs.hasOwnProperty('id')){
+					var divId = div.attribs.id;
+					div = $(div);
+					var data = div.children('.right').text().myTrim();
+					switch (divId){
+						case 'LeftMainContent__productDetail_contactName':
+							owner.fullname = data;
+							break;
+						case 'LeftMainContent__productDetail_contactAddress':
+							owner.address = data;
+							break;
+						case 'LeftMainContent__productDetail_contactPhone':
+							owner.phone = data;
+							break;
+						case 'LeftMainContent__productDetail_contactMobile':
+							owner.mobile = data;
+							break;
+						case 'LeftMainContent__productDetail_contactEmail':
+							var emailData = dec2str(div.children('.right').text().myTrim());
+							var match = /\b[\w0-9_]+@([\w0-9_]+\.)+[\w]+/.exec(emailData);
+							if (match.length > 0)
+								owner.email = match[0];
+							break;
+
+					}
+				}
+			}
+			result.owner = owner;
+
+			// crawl images
+			var houseImages = $('#thumbs').children();
+			var images = [];
+			for (var i = 0; i < houseImages.length; i++) {
+				var imageUrl = $(houseImages[i]).children()[0].attribs.src;
+				images.push(imageUrl.replace('80x60', '745x510'));
+			}
+			result.images = images;
+			
+			results.push(result);
+			// console.log(result);
 		}
 		else{
 			console.log('error');
 		}
 		crawl(index + 1);
 	})
+}
+
+function hex2str (hexstr) {
+	while (hexstr.indexOf('&#x') > -1){
+		var start = hexstr.indexOf('&#x');
+		var stop = hexstr.indexOf(';', start);
+		var hex = '0' + hexstr.substring(start + 2, stop);
+		// console.log("hex = " + hex);
+		var result = hexstr.substring(0, start);
+		result += String.fromCharCode(hex);
+		result += hexstr.substring(stop + 1);
+		hexstr = result;
+	}
+	return hexstr;
+}
+
+function dec2str (decstr) {
+	while (decstr.indexOf('&#') > -1){
+		var start = decstr.indexOf('&#');
+		var stop = decstr.indexOf(';', start);
+		var dec = decstr.substring(start + 2, stop);
+		// console.log("dec = " + dec);
+		var result = decstr.substring(0, start);
+		result += String.fromCharCode(dec);
+		result += decstr.substring(stop + 1);
+		decstr = result;
+	}
+	return decstr;
+}
+
+function saveToDB (i) {
+	if (i >= results.length){
+		connection.end();
+		return;
+	}
+	connection.query(
+		'INSERT INTO test (des) VALUES (?)',
+		[results[i].description],
+		function (err, result) {
+			if (err){
+				console.log(err);
+			}
+			else {
+				console.log('ok');
+			}
+			saveToDB(i + 1);
+		}
+	)
+	return;
 }
