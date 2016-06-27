6eef796094e67f6a62c7eb48bfa17d9fb2520163
Ngoc Don <kaitoukid2701@gmail.com>
2016-06-27 12:46:17 +0700

    Crawl images from batdongsan

--- crawler/nodejs-crawler/crawl-images-update-db.js
+++ crawler/nodejs-crawler/crawl-images-update-db.js
@@ -1,5 +1,9 @@
 var request = require('request');
 var fs = require('fs');
+var multer = require('multer');
+var bcrypt = require('bcrypt');
+var CryptoJS = require('crypto-js');
+var async = require('async');
 
 var mysql = require('mysql');
 var config = require('./config.js').MYSQL;
@@ -10,7 +14,11 @@ var connection = mysql.createConnection({
 	database: config.MYSQL_DB
 });
 
-var LOCAL_IMAGES_REGEX = '^public/uploads/images';
+var UPLOAD_DESTINATION = 'public/uploads/images';
+var LOCAL_IMAGES_REGEX = '^' + UPLOAD_DESTINATION;
+var uploadImages = multer({dest: UPLOAD_DESTINATION});
+
+var stt = 0;
 
 connection.query(
 	'SELECT * FROM images WHERE url NOT REGEXP ?',
@@ -20,9 +28,75 @@ connection.query(
 			console.log(err);
 		}
 		if (images.length > 0){
-			// pass
+			console.log('len: ' + images.length);
+			processImages(images, function () {
+				var interval = setInterval(function () {
+					console.log(stt + "/" + images.length);
+					if (stt >= images.length){
+						console.log('hehe' + stt + " " + images.length);
+						clearInterval(interval);
+						connection.end();
+						console.log('done');
+					}
+				}, 2000);
+			});
 		}
-		connection.end();
-		console.log('done');
 	}
 )
+
+function processImages (images, callback) {
+	var fns = [];
+	function createCB (index) {
+		return function (cb) {
+			processSingleImage(images, index, callback, cb);
+		}
+	}
+	for (var i = 0; i < images.length; i++) {
+		fns.push(createCB(i));
+	}
+
+	async.parallelLimit(fns, 20, function (err, results) {
+		if (err){
+			console.log(err);
+		}
+		callback();
+	})
+
+}
+
+function processSingleImage (images, index, callback, cb) {
+	if (index.length >= images.length){
+		// callback();
+		return
+	}
+	console.log(index + ":" + images.length);
+	// console.log(images[index]);
+	request(images[index].url, {encoding: 'binary'}, function (err, response, body) {
+		if (err){
+			console.log(err);
+		}
+		// console.log(response.statusCode);
+		// console.log(response.headers);
+		// console.log(body);
+		if (response.statusCode == 200){
+			var fileName = UPLOAD_DESTINATION + '/' + CryptoJS.MD5(bcrypt.genSaltSync(100)).toString();
+			fs.writeFileSync(fileName, body, 'binary');
+			connection.query(
+				'UPDATE images SET url = ? WHERE id = ?',
+				[fileName, images[index].id],
+				function (err, results) {
+					if (err){
+						console.log(images[index].id + ": error while updating database");
+						console.log(err);
+					}
+					stt++;
+					cb();
+				}
+			)
+		}
+		else{
+			stt++;
+			cb();
+		}
+	});
+}
--- crawler/nodejs-crawler/test.js
+++ crawler/nodejs-crawler/test.js
@@ -3,36 +3,36 @@ var fs = require('fs');
 
 var data = JSON.parse(fs.readFileSync('batdongsan1.json'));
 
-request.post('http://batdongsan.com.vn/HandlerWeb/redirect.ashx?IsMainSearch=true', {
-	form: {
-		'cboCity': "HN",
-		// cboCategory: 49,
-		cboDistrict: 2,
-		cboTypeRe: 326,
-		// cboWard: data.wards[841].bdsWardId,
-		cboWard: 9376,
-		cboArea: -1,
-		cboPrice: -1,
-		cboBedRoom: -1,
-		cboHomeDirection: -1
-	}
-}, function (err, response, body) {
-	if (err){
-		console.log(err);
-		return;
-	}
-	console.log(response.headers);
-})
-
-// request('http://file4.batdongsan.com.vn/resize/745x510/2016/06/21/20160621071824-c789.jpg', {encoding: 'binary'}, function (err, response, body) {
+// request.post('http://batdongsan.com.vn/HandlerWeb/redirect.ashx?IsMainSearch=true', {
+// 	form: {
+// 		'cboCity': "HN",
+// 		// cboCategory: 49,
+// 		cboDistrict: 2,
+// 		cboTypeRe: 326,
+// 		// cboWard: data.wards[841].bdsWardId,
+// 		cboWard: 9376,
+// 		cboArea: -1,
+// 		cboPrice: -1,
+// 		cboBedRoom: -1,
+// 		cboHomeDirection: -1
+// 	}
+// }, function (err, response, body) {
 // 	if (err){
 // 		console.log(err);
+// 		return;
 // 	}
-// 	console.log(response.statusCode);
 // 	console.log(response.headers);
-// 	console.log(body);
-// 	fs.writeFileSync('downloaded.jpg', body, 'binary');
-// });
+// })
+
+request('http://file4.batdongsan.com.vn/resize/745x510/2016/06/21/20160621071824-c789.jpg', {encoding: 'binary'}, function (err, response, body) {
+	if (err){
+		console.log(err);
+	}
+	console.log(response.statusCode);
+	console.log(response.headers);
+	// console.log(body);
+	fs.writeFileSync('downloaded.jpg', body, 'binary');
+});
 
 // var data = JSON.parse(fs.readFileSync('data.json'));
 // var count = 0;
