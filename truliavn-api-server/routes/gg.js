5e98257173e20bce915ae091a9b22f98ddbd2fa9
duckien95 <keanronal@gmail.com>
2016-07-01 23:25:22 +0700

    add google API distance

--- truliavn-api-server/routes/gg.js
+++ truliavn-api-server/routes/gg.js
@@ -3,24 +3,50 @@ var API_KEY = require('../config/apikey.js').GOOGLE_MAP_API_KEY;
 
 module.exports = function (router, connection, CITIES, DISTRICTS, WARDS) {
 
-router.post('/nearby', function (req, res) {
-	var lat = parseFloat(req.body.lat);
-	var lon = parseFloat(req.body.lon);
-	var radius = parseInt(req.body.radius);
-	var type = req.body.type;
-	var url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" + lat + "," + lon + "&radius=" + radius + "&types=" + type + "&key=" + API_KEY;
-	request(url, function (err, response, body) {
-		if (err || response.statusCode != 200){
-			return res.status(200).json({
-				status: 'error',
-				error: 'Request to Google error'
+	router.post('/nearby', function (req, res) {
+		var lat = parseFloat(req.body.lat);
+		var lon = parseFloat(req.body.lon);
+		var radius = parseInt(req.body.radius);
+		var type = req.body.type;
+		var url = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=" 
+					+ lat + "," + lon 
+					+ "&radius=" + radius 
+					+ "&types=" + type 
+					+ "&key=" + API_KEY;
+		request(url, function (err, response, body) {
+			if (err || response.statusCode != 200){
+				return res.status(200).json({
+					status: 'error',
+					error: 'Request to Google error'
+				})
+			}
+			res.status(200).json({
+				status: 'success',
+				results: JSON.parse(body)
 			})
-		}
-		res.status(200).json({
-			status: 'success',
-			results: JSON.parse(body)
 		})
 	})
-})
 
+	router.post('/distance', function (req, res) {
+		var origin = req.body.origin;
+		var destination = req.body.destination;
+		var url = "https://maps.googleapis.com/maps/api/distancematrix/json?units=metric&origins="
+		 		+ origin 
+		 		+ "&destinations="
+		 		+ destination
+		 		+ "&key="+ API_KEY;
+		 console.log(url);
+		request(url, function (err, response, body) {
+			if (err || response.statusCode != 200){
+				return res.status(200).json({
+					status : 'error',
+					error : 'Request fail'
+				})
+			}
+			res.status(200).json({
+				status : 'success',
+				results : JSON.parse(body)
+			})
+		})
+	})
 }
