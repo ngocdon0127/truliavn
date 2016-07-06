018a14fc7a102a3bd9bdbc0278b3ada5180fcfba
Ngoc Don <kaitoukid2701@gmail.com>
2016-07-06 13:59:45 +0700

    Update bdsId to local database

---
+++ crawler/nodejs-crawler/update-bds-id.js
@@ -0,0 +1,27 @@
+var request = require('request');
+var cheerio = require('cheerio');
+var fs = require('fs');
+var mysql = require('mysql');
+var MYSQL = require('./database.js').MYSQL;
+
+var connection = MYSQL();
+
+var data = JSON.parse(fs.readFileSync('batdongsan1.json'));
+
+var districts = data.districts;
+var wards = data.wards;
+
+var d = "";
+
+for (i in districts){
+	d += "UPDATE districts SET bdsDistrictId = " + districts[i].bdsDistrictId + " WHERE id = " + i + ";\n";
+}
+
+var w = "";
+
+for (i in wards){
+	w += "UPDATE wards SET bdsWardId = " + wards[i].bdsWardId + " WHERE id = " + i + ";\n";
+}
+
+fs.writeFileSync('sql.txt', d + w);
+console.log('done');
--- truliavn-api-server/routes/index.js
+++ truliavn-api-server/routes/index.js
@@ -4,21 +4,7 @@ var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-	res.render('index', { title: 'Express' });
+	res.render('index', { title: 'TruliaVN' });
 });
-
-router.get('/addhouse', function (req, res) {
-	res.render('addhouse');
-})
-
-router.get('/edithouse/:houseId', function (req, res) {
-	request('http://localhost:3000/api/getfeatures', function (err, response, body) {
-		if (!err && response.statusCode == 200){
-			// res.json(JSON.parse(body).features);
-			// return;
-			res.render('edithouse', {features: JSON.parse(body).features, houseId: req.params.houseId});
-		}
-	})
-})
 
 module.exports = router;
